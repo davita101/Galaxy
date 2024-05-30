165e465015cbe77f010f1c34d64185f139cf512a
davita101 <chemsfexs@gmail.com>
2024-05-30 18:01:00 +0400

    Update Home Navbar and index.css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,21 +4,21 @@ import { herobg, heromob, herotab } from '../assets'
 
 function Home() {
     return (
-        <div className=' w-full h-full'>
-            <div className='flex flex-wrap items-end justify-center'>
-                <div className='text-[white] pt-[25.6rem] pb-0 p-[10.2rem]'>
+        <div className=' w-full h-[100vh] flex lg:text-start text-center lg:p-[10.5rem] p-[1.5rem]'>
+            <div className='flex lg:flex-row flex-col lg:gap-0 gap-[2rem] lg:items-end items-center lg:justify-between justify-end w-full'>
+                <div className='text-[white] pb-0 grid gap-[1rem] '>
                     <h2 className='subheading-fs--100 font-thin ff-primary-100 primary-color--200'>{heroText[0].h2}</h2>
                     <h1 className='heading-fs--100 ff-primary-300'>{heroText[0].h1}</h1>
-                    <p className='paragraph-fs primary-color--200 ff-primary-200 max-w-[28rem]'>{heroText[0].p}</p>
+                    <p className=' paragraph-fs primary-color--200 ff-primary-200 sm:max-w-[28rem] max-w-[30rem]'>{heroText[0].p}</p>
                 </div>
 
-                <div className='button--hover w-[274px] h-[274px] border-[none]  heading-fs--400 relative ff-primary-300 bg-white rounded-full cursor-pointer text-black flex justify-center items-center ' >
+                <div className='button--hover lg:w-[274px] w-[205px] h-[205px] lg:h-[274px] border-[none]  heading-fs--400 relative ff-primary-300 bg-white rounded-full cursor-pointer text-black flex justify-center items-center ' >
                     EXPLORE
-                    <div className=' w-[274px] h-[274px]   bg-white rounded-full z-[-1] opacity-[.5] absolute' />
+                    <div className=' w-[205px] h-[205px] bg-white rounded-full z-[-1] opacity-[.5] absolute' />
                 </div>
-            </div>
+            </div >
 
-        </div>
+        </div >
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,25 +7,25 @@ import { FaX } from 'react-icons/fa6';
 
 
 function Navbar() {
-    const [active, setactive] = useState(0);
-    const [Click, SetCklick] = useState(true);
+    const [active, setActive] = useState(0);
+    const [Click, setClick] = useState(true);
 
-    const HendleaCtive = (index) => {
-        setactive(index)
+    const hendleActive = (index) => {
+        setActive(index)
     }
 
     return (
-        <div className='flex items-center justify-between pl-[1rem] pt-[2.5rem] fixed w-full'>
+        <div className='flex items-center justify-between lg:pl-[1rem] lg:pt-[2.5rem]  pl-[1rem] pt-[2.5rem] fixed w-full z-[99]'>
             <img src={Logo} alt="Website Logo" className='h-[3rem] w-[3rem]' />
-            <div onClick={() => SetCklick(!Click)} className='fixed lg:hidden flex right-[1rem] z-[4] text-[2rem]'>
+            <div onClick={() => setClick(!Click)} className='fixed sm:hidden flex right-[1rem] z-[4] text-[2rem]'>
                 {Click ? <FaBars className='text-white' /> : <FaX className='text-white' />}
             </div>
-            <div className='h-[.1rem] w-[473px] xl:bg-white bg-transparent relative bg-[#979797] opacity-[0.6] left-[4rem] rounded-md z-[5]' />
-            <div className='lg:flex hidden gap-[4rem]'>
-                <ul className='flex gap-[3.12rem] ml-[2rem] p-[3rem] pb-0 bg--color xl:pr-[10rem]  xl:pl-[12rem]  backdrop-blur-md  z-[3] relative border-white border-opacity-[0.01] items-center justify-end'>
+            <div className='h-[.1rem] w-[473px] lg:bg-white bg-transparent relative bg-[#979797] opacity-[0.6] left-[4rem] rounded-md z-[5]' />
+            <div className='sm:flex hidden gap-[4rem]'>
+                <ul className='flex gap-[3.12rem] ml-[2rem] p-[3rem] pb-0 bg--color lg:pr-[10rem]  lg:pl-[12rem]  backdrop-blur-md  z-[3] relative border-white border-opacity-[0.01] items-center justify-end'>
                     {navbar.map((item, index) => (
-                        <Link to={item.link}>
-                            <div key={index} onClick={() => HendleaCtive(index)} className='lg:flex hidden flex-col justify-between items-end gap-[0.68rem] h-[4rem] subheading-fs--300 cursor-pointer z-[3] text-white'>
+                        <Link key={index} to={item.link}>
+                            <div onClick={() => hendleActive(index)} className='sm:flex hidden flex-col justify-between items-end gap-[0.68rem] h-[4rem] subheading-fs--300 cursor-pointer z-[3] text-white'>
                                 <div className='flex gap-[0.68rem]'>
                                     <span className='font-bold'>{item.num}</span>
                                     <li>{item.li}</li>
@@ -37,11 +37,11 @@ function Navbar() {
                 </ul>
             </div>
             {/* mobile version */}
-            <div className="lg:hidden  flex fixed right-0 top-0 ">
-                <ul className={`${Click ? "translate-x-[500px]" : "translate-x-0"} flex gap-[2rem] flex-col pb-[7rem] bg--color pl-[3rem] relative xl:pl-[12rem]  backdrop-blur-md  w-[15rem] h-[100vh] z-[3] border-white border-opacity-[0.01] items-start justify-center `}>
+            <div className="sm:hidden flex fixed right-0 top-0 ">
+                <ul className={`${Click ? "translate-x-[250px]" : "translate-x-0"} transition-all flex gap-[2rem] flex-col pb-[7rem] bg--color pl-[3rem] relative lg:pl-[12rem]  backdrop-blur-md  w-[15rem] h-[100vh] z-[3] border-white border-opacity-[0.01] items-start justify-center `}>
                     {navbar.map((item, index) => (
-                        <Link to={item.link}>
-                            <div key={index} onClick={() => HendleaCtive(index)} className='flex flex-row w-[12rem] justify-between gap-[0.68rem] subheading--300 cursor-pointer z-[3] text-white'>
+                        <Link key={index} to={item.link}>
+                            <div onClick={() => { hendleActive(index), setClick(true) }} className='flex flex-row w-[12rem] justify-between gap-[0.68rem] subheading--300 cursor-pointer z-[3] text-white'>
                                 <div className='flex gap-[0.68rem]'>
                                     <span className='font-bold'>{item.num}</span>
                                     <li>{item.li}</li>
